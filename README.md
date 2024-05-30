0ff0a2ee0859de19e1ae8bbe014d5aca550134cb
Stan Girard <girard.stanislas@gmail.com>
2024-05-30 17:37:14 +0200

    chore: Update package description for LLM consumption (#2)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MegaParse - Your Mega Parser for every type of documents
 
 <div align="center">
-    <img src="./logo.png" alt="Quivr-logo" width="30%"  style="border-radius: 50%; padding-bottom: 20px"/>
+    <img src="https://raw.githubusercontent.com/QuivrHQ/MegaParse/main/logo.png" alt="Quivr-logo" width="30%"  style="border-radius: 50%; padding-bottom: 20px"/>
 </div>
 
 MegaParse is a powerful and versatile parser that can handle various types of documents with ease. Whether you're dealing with text, PDFs, Powerpoint presentations, Word documents MegaParse has got you covered. Focus on having no information loss during parsing.
@@ -12,3 +12,33 @@ MegaParse is a powerful and versatile parser that can handle various types of do
 - **Fast and Efficient**: Designed with speed and efficiency at its core.
 - **Wide File Compatibility**: Supports Text, PDF, Powerpoint presentations, Excel, CSV, Word documents.
 - **Open Source**: Freedom is beautiful, and so is MegaParse. Open source and free to use.
+
+
+## Installation
+
+```bash
+pip install megaparse
+```
+
+## Usage
+
+1. Create an account on [Llama Cloud](https://cloud.llamaindex.ai/) and get your API key.
+
+2. Create a new file in the root directory of the project and name it `.env`.
+
+3. Add the following line to the `.env` file and replace `llx-your_api_key` with your actual API key.
+
+```bash
+LLAMA_CLOUD_API_KEY=llx-your_api_key
+```
+
+4. Now you can use the following code to convert a PDF to Markdown and save it to a file.
+
+```python
+from megaparse import MegaParse
+
+megaparse = MegaParse(file_path="./test.pdf")
+content = megaparse.convert()
+print(content)
+megaparse.save_md(content, "./test.md")
+```
--- megaparse/Converter.py
+++ megaparse/Converter.py
@@ -1,3 +1,4 @@
+import os
 from docx.document import Document as DocumentObject
 from docx import Document
 from docx.table import Table
@@ -15,20 +16,22 @@ from typing import List, Set
 from llama_parse import LlamaParse
 from llama_parse.utils import ResultType, Language
 from llama_index.core.schema import Document as LlamaDocument
-from src.markdown_processor import MarkdownProcessor
+from markdown_processor import MarkdownProcessor
 
 import nest_asyncio
 
 nest_asyncio.apply()
 
+
 class Converter:
     def __init__(self) -> None:
         pass
+
     def convert(self, file_path: str) -> str:
         raise NotImplementedError("Subclasses should implement this method")
 
-    def save_md(self, md_content: str, file_path: Path| str) -> None:
-        with open(file_path, 'w') as f:
+    def save_md(self, md_content: str, file_path: Path | str) -> None:
+        with open(file_path, "w") as f:
             f.write(md_content)
 
 
@@ -36,7 +39,7 @@ class DOCXConverter(Converter):
     def __init__(self) -> None:
         pass
 
-    def convert(self, file_path : str ) -> str:
+    def convert(self, file_path: str) -> str:
         doc = Document(file_path)
         md_content = []
         # Handle header
@@ -52,8 +55,8 @@ class DOCXConverter(Converter):
                 md_content += self._handle_table(Table(element, doc))
             # Add more handlers here (image, header, footer, etc)
 
-        return '\n'.join(md_content)
-    
+        return "\n".join(md_content)
+
     def _handle_header(self, header) -> str:
         if not self.header_handled:
             parts = []
@@ -62,16 +65,16 @@ class DOCXConverter(Converter):
             for table in header.tables:
                 parts += self._handle_header_table(table)
             self.header_handled = True
-            return '\n'.join(parts)
-        return ''
-    
+            return "\n".join(parts)
+        return ""
+
     def _handle_header_table(self, table: Table) -> List[str]:
         cell_texts = [cell.text for row in table.rows for cell in row.cells]
-        cell_texts.remove('')
+        cell_texts.remove("")
         # Find the most repeated cell text
         text_counts = Counter(cell_texts)
         title = text_counts.most_common(1)[0][0] if cell_texts else ""
-        other_texts = [text for text in cell_texts if text != title and text != '']
+        other_texts = [text for text in cell_texts if text != title and text != ""]
 
         md_table_content = []
         if title:
@@ -79,24 +82,23 @@ class DOCXConverter(Converter):
         for text in other_texts:
             md_table_content.append(f"*{text}*;")
         return md_table_content
-    
+
     def _handle_paragraph(self, paragraph: Paragraph) -> str:
-        if paragraph.style.name.startswith('Heading'): #type: ignore
-            level = int(paragraph.style.name.split()[-1]) #type: ignore
+        if paragraph.style.name.startswith("Heading"):  # type: ignore
+            level = int(paragraph.style.name.split()[-1])  # type: ignore
             return f"{'#' * level} {paragraph.text}"
         else:
             parts = []
             for run in paragraph.runs:
-                if run.text != '':
+                if run.text != "":
                     parts.append(self._handle_run(run))
-            return ''.join(parts)
+            return "".join(parts)
 
-        
     def _handle_run(self, run: Run) -> str:
         text: str = run.text
         if run.bold:
             if len(text) < 200:
-                #FIXME : handle table needs to be improved -> have the paragraph they are in
+                # FIXME : handle table needs to be improved -> have the paragraph they are in
                 text = f"## {text}"
             else:
                 text = f"**{text}**"
@@ -106,26 +108,26 @@ class DOCXConverter(Converter):
 
     def _handle_table(self, table: Table) -> List[str]:
         row_content = []
-        for i,row in enumerate(table.rows):
-            row_content.append( '| ' + ' | '.join(cell.text.strip() for cell in row.cells) + ' |')
+        for i, row in enumerate(table.rows):
+            row_content.append(
+                "| " + " | ".join(cell.text.strip() for cell in row.cells) + " |"
+            )
             if i == 0:
-                row_content.append('|' + '---|' * len(row.cells))
+                row_content.append("|" + "---|" * len(row.cells))
 
         return row_content
-    
+
     def save_md(self, md_content: str, file_path: Path | str) -> None:
-        with open(file_path, 'w') as f:
+        with open(file_path, "w") as f:
             f.write(md_content)
 
 
-
 class PPTXConverter:
-    def __init__(self, add_images = False) -> None:
+    def __init__(self, add_images=False) -> None:
         self.header_handled = False
         self.add_images = add_images
 
-
-    def convert(self, file_path: str ) -> str:
+    def convert(self, file_path: str) -> str:
         prs = Presentation(file_path)
         md_content = []
         unique_slides: Set[str] = set()
@@ -139,20 +141,20 @@ class PPTXConverter:
         for i, slide in enumerate(prs.slides):
             slide_md_content: List[str] = []
             for shape in slide.shapes:
-                if shape.shape_type == MSO_SHAPE_TYPE.TABLE: # type: ignore
+                if shape.shape_type == MSO_SHAPE_TYPE.TABLE:  # type: ignore
                     slide_md_content += self._handle_table(shape.table)
-                elif shape.shape_type == MSO_SHAPE_TYPE.PICTURE and self.add_images: # type: ignore
+                elif shape.shape_type == MSO_SHAPE_TYPE.PICTURE and self.add_images:  # type: ignore
                     slide_md_content.append(self._handle_image(shape))
                 elif hasattr(shape, "text"):
                     slide_md_content.append(self._handle_paragraph(shape.text))
-            
-            slide_md_str = '\n'.join(slide_md_content)
+
+            slide_md_str = "\n".join(slide_md_content)
             if slide_md_str not in unique_slides:
                 unique_slides.add(slide_md_str)
                 slide_md_str = f"## Slide {i+1}\n{slide_md_str}"
                 md_content.append(slide_md_str)
 
-        return '\n'.join(md_content)
+        return "\n".join(md_content)
 
     def _handle_header(self, placeholders) -> str:
         if not self.header_handled:
@@ -163,56 +165,57 @@ class PPTXConverter:
                 elif placeholder.placeholder_format.idx == 1:  # Subtitle placeholder
                     parts.append(f"## {placeholder.text}")
             self.header_handled = True
-            return '\n'.join(parts)
-        return ''
+            return "\n".join(parts)
+        return ""
 
     def _handle_paragraph(self, text: str) -> str:
         # Assuming text is a simple paragraph without complex formatting
-        #if text contains letters return text
+        # if text contains letters return text
         if any(c.isalpha() for c in text):
-            return text + '\n'
-        return ''
+            return text + "\n"
+        return ""
 
     def _handle_image(self, shape) -> str:
         image = shape.image
         image_bytes = image.blob
         image_format = image.ext
-        image_filename = f'images/image_{shape.shape_id}.{image_format}'
-        with open(image_filename, 'wb') as f:
+        image_filename = f"images/image_{shape.shape_id}.{image_format}"
+        with open(image_filename, "wb") as f:
             f.write(image_bytes)
         return f"![Image {shape.shape_id}](../{image_filename})"
 
     def _handle_table(self, table) -> List[str]:
         row_content = []
         for i, row in enumerate(table.rows):
-            row_content.append('| ' + ' | '.join(cell.text.strip() for cell in row.cells) + ' |')
+            row_content.append(
+                "| " + " | ".join(cell.text.strip() for cell in row.cells) + " |"
+            )
             if i == 0:
-                row_content.append('|' + '---|' * len(row.cells))
+                row_content.append("|" + "---|" * len(row.cells))
         return row_content
 
     def save_md(self, md_content: str, file_path: Path | str) -> None:
-        with open(file_path, 'w') as f:
+        with open(file_path, "w") as f:
             f.write(md_content)
 
 
-
-
 class PDFConverter:
-    def __init__(self, api_key:str, handle_pagination: bool = True, handle_header: bool  = True) -> None:
+    def __init__(
+        self, api_key: str, handle_pagination: bool = True, handle_header: bool = True
+    ) -> None:
         self.handle_pagination = handle_pagination
         self.handle_header = handle_header
         self.api_key = api_key
 
         parsing_instructions = "Do not take into account the page breaks (no --- between pages), do not repeat the header and the footer so the tables are merged. Keep the same format for similar tables."
         self.parser = LlamaParse(
-            api_key=str(api_key), 
+            api_key=str(api_key),
             result_type=ResultType.MD,
             gpt4o_mode=True,
             verbose=True,
             language=Language.FRENCH,
             parsing_instruction=parsing_instructions,  # Optionally you can define a parsing instruction
         )
-
 
     def convert(self, file_path: str) -> str:
         documents: List[LlamaDocument] = self.parser.load_data(file_path)
@@ -221,11 +224,42 @@ class PDFConverter:
         if not (self.handle_pagination or self.handle_header):
             return parsed_md
         else:
-            md_processor = MarkdownProcessor(parsed_md, strict = self.handle_header, remove_pagination = self.handle_pagination)
+            md_processor = MarkdownProcessor(
+                parsed_md,
+                strict=self.handle_header,
+                remove_pagination=self.handle_pagination,
+            )
             md_cleaned = md_processor.process()
             return md_cleaned
 
+    def save_md(self, md_content: str, file_path: Path | str) -> None:
+        with open(file_path, "w") as f:
+            f.write(md_content)
 
-    def save_md(self, md_content: str, file_path: Path| str) -> None:
-        with open(file_path, 'w') as f:
+
+class MegaParse:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        self.api_key = os.getenv("LLAMA_CLOUD_API_KEY")
+
+    def convert(self) -> str:
+        file_extension: str = os.path.splitext(self.file_path)[1]
+
+        if file_extension == ".docx":
+            converter = DOCXConverter(
+                file_path=self.file_path, file_extension=file_extension
+            )
+        elif file_extension == ".pptx":
+            converter = PPTXConverter(
+                file_path=self.file_path, file_extension=file_extension
+            )
+        elif file_extension == ".pdf":
+            converter = PDFConverter(api_key=self.api_key)
+        else:
+            print(self.file_path, file_extension)
+            raise ValueError(f"Unsupported file extension: {file_extension}")
+        return converter.convert(self.file_path)
+
+    def save_md(self, md_content: str, file_path: Path | str) -> None:
+        with open(file_path, "w") as f:
             f.write(md_content)
