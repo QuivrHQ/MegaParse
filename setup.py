9fec7c7c6933ebb0b73296f23cffbed182b15bad
Stan Girard <girard.stanislas@gmail.com>
2024-05-30 16:41:06 +0200

    chore: Update setup.py with long description and content type

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="megaparse",
     version="0.1.0",
     description="A package for parsing various file formats",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Quivr",
     packages=find_packages(),
     install_requires=[
