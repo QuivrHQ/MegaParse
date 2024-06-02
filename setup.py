c8d9fdca166e7b0c1387ab5aaa61cfb9d2a369e1
Stan Girard <girard.stanislas@gmail.com>
2024-06-02 12:35:49 +0200

    chore(main): release 0.0.5 (#11)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="megaparse",
-    version="0.0.4",
+    version="0.0.5",
     description="Parse complex files (PDF,Docx,PPTX) for LLM consumption",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
