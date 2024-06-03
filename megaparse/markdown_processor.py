20cc56291162ae87c7d97ea8771f2ce1d58019b8
Chloé Daems <73901882+chloedia@users.noreply.github.com>
2024-06-03 12:03:40 +0200

    add: gpt cleaner for header and footer (#13)

--- megaparse/markdown_processor.py
+++ megaparse/markdown_processor.py
@@ -1,6 +1,8 @@
 import os
 from collections import Counter
 from typing import LiteralString
+from langchain_openai import ChatOpenAI
+
 
 class MarkdownProcessor:
     def __init__(self, md_result: str, strict: bool, remove_pagination: bool):
@@ -67,25 +69,54 @@ class MarkdownProcessor:
         with open(output_path, "w") as f:
             f.write(cleaned_result)
 
-    def process(self):
+    def remove_header_llm(self):
+        llm = ChatOpenAI(model="gpt-4o")
+        # Define the prompt
+        messages = [
+            (
+                "system",
+                "You are a document cleaner and you are used to remove repetitive headers / footer from parsed files in markdown.",
+            ),
+        ]
+
+        prompt = f"""You are a document cleaner and you are used to remove repetitive headers / footer from parsed files in markdown.
+        Here is a md file : "{self.md_result}"
+        I want you to identify repetitive texts that could be associate to a document header and footer. Please identify the headers, the footer and remove them from the document.
+        Answer with only the cleaned document in markdown format. 
+        Result : """
+
+        messages.append(("human", self.md_result)) #type: ignore
+
+        result = llm.invoke(messages)
+
+        return result.content
+
+
+
+    def process(self, gpt4o_cleaner = False):
         """Process the markdown result by removing duplicate paragraphs and headers."""
-        pages = self.split_into_pages()
-        paragraphs = self.split_into_paragraphs(pages)
-        #other_pages_paragraphs = self.split_into_paragraphs(pages[1:])
 
-        cleaned_paragraphs, duplicate_paragraphs = self.remove_duplicates(paragraphs)
-        header_components_count = self.identify_header_components(duplicate_paragraphs)
+        if gpt4o_cleaner:
+            cleaned_result = self.remove_header_llm()
 
-        if self.strict:
-            final_paragraphs = self.remove_header_lines(cleaned_paragraphs[5:], header_components_count)
-            final_paragraphs = cleaned_paragraphs[:5] + final_paragraphs
         else:
-            final_paragraphs = cleaned_paragraphs
+            pages = self.split_into_pages()
+            paragraphs = self.split_into_paragraphs(pages)
+            #other_pages_paragraphs = self.split_into_paragraphs(pages[1:])
 
-        # Combine first page paragraphs with cleaned paragraphs from other pages
-        all_paragraphs = final_paragraphs
-        cleaned_result = "\n\n".join(all_paragraphs)
+            cleaned_paragraphs, duplicate_paragraphs = self.remove_duplicates(paragraphs)
+            header_components_count = self.identify_header_components(duplicate_paragraphs)
 
-        cleaned_result = self.merge_tables(cleaned_result)
+            if self.strict:
+                final_paragraphs = self.remove_header_lines(cleaned_paragraphs[5:], header_components_count)
+                final_paragraphs = cleaned_paragraphs[:5] + final_paragraphs
+            else:
+                final_paragraphs = cleaned_paragraphs
+
+            # Combine first page paragraphs with cleaned paragraphs from other pages
+            all_paragraphs = final_paragraphs
+            cleaned_result = "\n\n".join(all_paragraphs)
+
+        cleaned_result = self.merge_tables(str(cleaned_result))
         return cleaned_result
 
