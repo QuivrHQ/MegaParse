717d5c4f9a78b5967039dfede44d05040ec6ce2b
dSupertramp <salvatoredanilopalumbo@gmail.com>
2024-06-26 10:26:19 +0200

    Add OpenAI key to ChatOpenAI

--- megaparse/multimodal_convertor/megaparse_vision.py
+++ megaparse/multimodal_convertor/megaparse_vision.py
@@ -8,11 +8,13 @@ import base64
 from pdf2image import convert_from_path
 import asyncio
 import re
+import os
+from dotenv import load_dotenv
 
 # BASE_OCR_PROMPT = """
-# Transcribe the content of this file into markdown. Be mindful of the formatting. 
+# Transcribe the content of this file into markdown. Be mindful of the formatting.
 # Add formatting if you think it is not clear.
-# Do not include page breaks and merge content of tables if it is continued in the next page. 
+# Do not include page breaks and merge content of tables if it is continued in the next page.
 # Add tags around what you identify as a table [TABLE], header - complete chain of characters that are repeated at each start of pages - [HEADER], table of content [TOC] in the format '[tag] ... [/tag]'
 # Return only the parsed content.
 # """
@@ -47,13 +49,17 @@ Follow these instructions to complete the task:
 10. Return only the parsed content in markdown format, including the specified tags for tables, headers, and table of contents.
 """
 
+
 class ModelEnum(str, Enum):
     """Model to use for the conversion"""
+
     CLAUDE = "claude-3.5"
     GPT4O = "gpt-4o"
 
+
 class TagEnum(str, Enum):
     """Possible tags for the elements in the file"""
+
     TABLE = "TABLE"
     TOC = "TOC"
     HEADER = "HEADER"
@@ -61,20 +67,22 @@ class TagEnum(str, Enum):
 
 
 class MegaParseVision:
+    load_dotenv()
+
     def __init__(self, model: ModelEnum = ModelEnum.GPT4O):
         if model == ModelEnum.GPT4O:
-            self.model = ChatOpenAI(model="gpt-4o")
+            self.model = ChatOpenAI(model="gpt-4o", api_key=os.getenv("OPENAI_API_KEY"))
         elif model == ModelEnum.CLAUDE:
             raise NotImplementedError("Claude support not yet implemented")
         else:
             raise ValueError(f"Model {model} not supported")
-        
-        self.parsed_chunks: list[str]| None = None
-        
-    def process_file(self, file_path: str, image_format: str = 'PNG') -> List[str]:
+
+        self.parsed_chunks: list[str] | None = None
+
+    def process_file(self, file_path: str, image_format: str = "PNG") -> List[str]:
         """
         Process a PDF file and convert its pages to base64 encoded images.
-        
+
         :param file_path: Path to the PDF file
         :param image_format: Format to save the images (default: PNG)
         :return: List of base64 encoded images
@@ -91,18 +99,18 @@ class MegaParseVision:
         except Exception as e:
             raise ValueError(f"Error processing PDF file: {str(e)}")
 
-    def get_element(self, tag : TagEnum, chunk: str):
-        pattern = rf'\[{tag.value}\]([\s\S]*?)\[/{tag.value}\]'
+    def get_element(self, tag: TagEnum, chunk: str):
+        pattern = rf"\[{tag.value}\]([\s\S]*?)\[/{tag.value}\]"
         all_elmts = re.findall(pattern, chunk)
         if not all_elmts:
             print(f"No {tag.value} found in the chunk")
             return []
         return [elmt.strip() for elmt in all_elmts]
-        
+
     async def send_to_mlm(self, images_data: List[str]) -> str:
         """
         Send images to the language model for processing.
-        
+
         :param images_data: List of base64 encoded images
         :return: Processed content as a string
         """
@@ -110,7 +118,8 @@ class MegaParseVision:
             {
                 "type": "image_url",
                 "image_url": {"url": f"data:image/jpeg;base64,{image_data}"},
-            } for image_data in images_data
+            }
+            for image_data in images_data
         ]
         message = HumanMessage(
             content=[
@@ -120,21 +129,24 @@ class MegaParseVision:
         )
         response = await self.model.ainvoke([message])
         return str(response.content)
-    
+
     async def parse(self, file_path: str, batch_size: int = 3) -> str:
         """
         Parse a PDF file and process its content using the language model.
-        
+
         :param file_path: Path to the PDF file
         :param batch_size: Number of pages to process concurrently
         :return: List of processed content strings
         """
         pdf_base64 = self.process_file(file_path)
-        tasks = [self.send_to_mlm(pdf_base64[i:i+batch_size]) for i in range(0, len(pdf_base64), batch_size)]
+        tasks = [
+            self.send_to_mlm(pdf_base64[i : i + batch_size])
+            for i in range(0, len(pdf_base64), batch_size)
+        ]
         self.parsed_chunks = await asyncio.gather(*tasks)
         responses = self.get_cleaned_content("\n".join(self.parsed_chunks))
         return responses
-    
+
     def get_cleaned_content(self, parsed_file: str) -> str:
         """
         Get cleaned parsed file without any tags defined in TagEnum.
@@ -149,34 +161,34 @@ class MegaParseVision:
             str: The cleaned content without TagEnum tags.
 
         """
-        tag_pattern = '|'.join(map(re.escape, TagEnum.__members__.values()))
-        tag_regex = rf'\[({tag_pattern})\](.*?)\[/\1\]'
+        tag_pattern = "|".join(map(re.escape, TagEnum.__members__.values()))
+        tag_regex = rf"\[({tag_pattern})\](.*?)\[/\1\]"
         # handle the HEADER tag specially
-        header_pattern = rf'\[{TagEnum.HEADER.value}\](.*?)\[/{TagEnum.HEADER.value}\]'
+        header_pattern = rf"\[{TagEnum.HEADER.value}\](.*?)\[/{TagEnum.HEADER.value}\]"
         headers = re.findall(header_pattern, parsed_file, re.DOTALL)
         if headers:
             first_header = headers[0].strip()
             # Remove all HEADER tags and their content
-            parsed_file = re.sub(header_pattern, '', parsed_file, flags=re.DOTALL)
+            parsed_file = re.sub(header_pattern, "", parsed_file, flags=re.DOTALL)
             # Add the first header back at the beginning
             parsed_file = f"{first_header}\n{parsed_file}"
 
         # Remove all other tags
         def remove_tag(match):
             return match.group(2)
+
         cleaned_content = re.sub(tag_regex, remove_tag, parsed_file, flags=re.DOTALL)
 
-        cleaned_content = re.sub(r'^```.*$\n?', '', cleaned_content, flags=re.MULTILINE)
-        cleaned_content = re.sub(r'\n\s*\n', '\n\n', cleaned_content)
+        cleaned_content = re.sub(r"^```.*$\n?", "", cleaned_content, flags=re.MULTILINE)
+        cleaned_content = re.sub(r"\n\s*\n", "\n\n", cleaned_content)
         cleaned_content = cleaned_content.replace("|\n\n|", "|\n|")
         cleaned_content = cleaned_content.strip()
-        
+
         return cleaned_content
 
 
 if __name__ == "__main__":
     parser = MegaParseVision()
-    responses = asyncio.run(parser.parse("megaparse/tests/input_tests/MegaFake_report.pdf"))
+    responses = asyncio.run(parser.parse("../tests/input_tests/MegaFake_report.pdf"))
     print(responses)
     print("Done!")
-
