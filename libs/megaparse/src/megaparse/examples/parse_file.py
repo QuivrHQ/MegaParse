eea6cfd1dc4c1fb863b8040b1e279871c1f0a8fa
chloedia <chloedaems0@gmail.com>
2024-12-10 00:48:38 +0100

    add: structured output formatter

--- libs/megaparse/src/megaparse/examples/parse_file.py
+++ libs/megaparse/src/megaparse/examples/parse_file.py
@@ -1,13 +1,28 @@
 from megaparse.formatter.unstructured_formatter.md_formatter import MarkDownFormatter
 from megaparse.megaparse import MegaParse
+from megaparse.formatter.structured_formatter.custom_structured_formatter import (
+    CustomStructuredFormatter,
+)
 from megaparse.parser.unstructured_parser import UnstructuredParser
+
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+
+class MyCustomFormat(BaseModel):
+    title: str = Field(description="The title of the document.")
+    problem: str = Field(description="The problem statement.")
+    solution: str = Field(description="The solution statement.")
+
 
 if __name__ == "__main__":
     # Parse a file
     parser = UnstructuredParser()
-    formatter = MarkDownFormatter()
+    model = ChatOpenAI()
+    formatter_1 = MarkDownFormatter()
+    formatter_2 = CustomStructuredFormatter(model=model, output_model=MyCustomFormat)
 
-    megaparse = MegaParse(parser=parser, formatters=[formatter])
+    megaparse = MegaParse(parser=parser, formatters=[formatter_1, formatter_2])
 
     file_path = "libs/megaparse/tests/pdf/sample_pdf.pdf"
     result = megaparse.load(file_path=file_path)
---
+++ libs/megaparse/src/megaparse/formatter/structured_formatter/__init__.py
@@ -0,0 +1,16 @@
+from langchain_core.language_models.chat_models import BaseChatModel
+from megaparse.formatter.base import BaseFormatter
+from pydantic import BaseModel
+
+
+class StructuredFormatter(BaseFormatter):
+    def __init__(self, model: BaseChatModel, output_model: type[BaseModel]):
+        super().__init__(model)
+        self.output_model = output_model
+
+    async def format_string(
+        self,
+        text: str,
+        file_path: str | None = None,
+    ) -> str:  # FIXME: Return a structured output of type BaseModel ?
+        raise NotImplementedError()
