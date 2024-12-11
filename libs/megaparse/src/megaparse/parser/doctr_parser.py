c7d0cbdc2a39dd4dcbeda433a93f0e6df9ce88dd
aminediro <aminedirhoussi@gmail.com>
2024-12-11 17:51:53 +0100

    doctr

--- libs/megaparse/src/megaparse/parser/doctr_parser.py
+++ libs/megaparse/src/megaparse/parser/doctr_parser.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from typing import IO, BinaryIO, List
 
@@ -7,6 +8,8 @@ from onnxtr.io import DocumentFile
 from onnxtr.models import EngineConfig, ocr_predictor
 
 from megaparse.parser.base import BaseParser
+
+logger = logging.getLogger("megaparse")
 
 
 class DoctrParser(BaseParser):
@@ -23,9 +26,10 @@ class DoctrParser(BaseParser):
     ):
         self.use_gpu = use_gpu
         general_options = rt.SessionOptions()
+        providers = self._get_providers()
         engine_config = EngineConfig(
             session_options=general_options,
-            providers=["CUDAExecutionProvider", "CPUExecutionProvider"],
+            providers=providers,
         )
         # TODO: set in config or pass as kwargs
         self.predictor = ocr_predictor(
@@ -44,13 +48,14 @@ class DoctrParser(BaseParser):
 
     def _get_providers(self) -> List[str]:
         prov = rt.get_available_providers()
+        logger.info("Available providers:", prov)
         if self.use_gpu:
             # TODO: support openvino, directml etc
             if "CUDAExecutionProvider" not in prov:
                 raise ValueError(
                     "onnxruntime can't find CUDAExecutionProvider in list of available providers"
                 )
-            return ["CUDAExecutionProvider", "CPUExecutionProvider"]
+            return ["CUDAExecutionProvider"]
         else:
             return ["CPUExecutionProvider"]
 
