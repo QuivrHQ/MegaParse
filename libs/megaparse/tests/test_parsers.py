6517f522970de3ce1127bdc6b39ca2d509abe268
chloedia <chloedaems0@gmail.com>
2024-12-16 16:19:14 +0100

    fix: add parse tests for every supported extensions

---
+++ libs/megaparse/tests/test_parsers.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+from megaparse.parser.doctr_parser import DoctrParser
+from megaparse.parser.llama import LlamaParser
+from megaparse.parser.megaparse_vision import MegaParseVision
+from megaparse.parser.unstructured_parser import UnstructuredParser
+from megaparse_sdk.schema.extensions import FileExtension
+
+PARSER_LIST = [
+    UnstructuredParser,
+    DoctrParser,
+]
+
+
+@pytest.mark.parametrize("parser", PARSER_LIST)
+@pytest.mark.parametrize("extension", list(FileExtension))
+def test_sync_parser(parser, extension):
+    directory = "./tests/supported_docs"
+    file_path = next(
+        (
+            os.path.join(root, file)
+            for root, _, files in os.walk(directory)
+            for file in files
+            if file.endswith(extension.value)
+        ),
+        None,
+    )
+    if file_path is None:
+        pytest.fail(f"No file with extension {extension.value} found in {directory}")
+
+    response = parser().convert(file_path)
+
+    assert response
+    assert len(response) > 0
