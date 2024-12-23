b22198e3bba1d8cb41d9d5055406692c0ed5a633
chloedia <chloedaems0@gmail.com>
2024-12-23 11:09:51 +0100

    add: native tests

--- libs/megaparse/tests/pdf/test_detect_ocr.py
+++ libs/megaparse/tests/pdf/test_detect_ocr.py
@@ -4,12 +4,17 @@ import pytest
 from megaparse.parser.strategy import determine_strategy
 from megaparse_sdk.schema.parser_config import StrategyEnum
 
-list_pdf = os.listdir("./tests/pdf/ocr")
+ocr_pdfs = os.listdir("./tests/pdf/ocr")
+native_pdfs = os.listdir("./tests/pdf/native")
 
 
-@pytest.mark.parametrize("pdf", list_pdf)
-def test_strategy_all(pdf):
-    strategy = determine_strategy(
-        f"./tests/pdf/ocr/{pdf}", threshold_pages_ocr=0.2, threshold_image_page=0.3
-    )
+@pytest.mark.parametrize("hi_res_pdf", ocr_pdfs)
+def test_hi_res_strategy(hi_res_pdf):
+    strategy = determine_strategy(f"./tests/pdf/ocr/{hi_res_pdf}")
     assert strategy == StrategyEnum.HI_RES
+
+
+@pytest.mark.parametrize("native_pdf", native_pdfs)
+def test_fast_strategy(native_pdf):
+    strategy = determine_strategy(f"./tests/pdf/native/{native_pdf}")
+    assert strategy == StrategyEnum.FAST
