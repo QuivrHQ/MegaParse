351b63af9269a743462034a3406d4b7f83cb7185
chloedia <chloedaems0@gmail.com>
2025-01-06 18:01:06 +0100

    feat: new basemodel for document

--- libs/megaparse/src/megaparse/examples/parse_file.py
+++ libs/megaparse/src/megaparse/examples/parse_file.py
@@ -1,6 +1,10 @@
 import asyncio
+from typing import List
 
 from langchain_openai import ChatOpenAI
+from llama_index.core.schema import Document as LlamaDocument
+from llama_parse import LlamaParse
+from llama_parse.utils import Language, ResultType
 from megaparse.formatter.structured_formatter.custom_structured_formatter import (
     CustomStructuredFormatter,
 )
@@ -9,11 +13,6 @@ from megaparse.megaparse import MegaParse
 from megaparse.parser.doctr_parser import DoctrParser
 from megaparse.parser.unstructured_parser import UnstructuredParser
 from pydantic import BaseModel, Field
-
-from llama_parse import LlamaParse
-from llama_parse.utils import Language, ResultType
-from typing import List
-from llama_index.core.schema import Document as LlamaDocument
 
 
 class MyCustomFormat(BaseModel):
--- libs/megaparse/src/megaparse/models/document.py
+++ libs/megaparse/src/megaparse/models/document.py
@@ -1,7 +1,88 @@
 from typing import Dict, List
 
 from pydantic import BaseModel
-from unstructured.documents.elements import Element
+
+
+class Block(BaseModel):
+    """
+    A class to represent a block.
+    Really Simplified.
+    """
+
+    metadata: Dict  # FIXME: To be defined as a pydantic model later @Amine
+    content: str
+
+
+class TextBlock(Block):
+    """
+    A class to represent a text block.
+    Really Simplified.
+    """
+
+    pass
+
+
+class ImageBlock(Block):
+    """
+    A class to represent an image block.
+    Really Simplified.
+    """
+
+    pass
+
+
+class TitleBlock(Block):
+    """
+    A class to represent a title block.
+    Really Simplified.
+    """
+
+    pass
+
+
+class SubTitle(Block):
+    """
+    A class to represent a subtitle block.
+    Really Simplified.
+    """
+
+    depth: int
+
+
+class TableBlock(Block):
+    """
+    A class to represent a table block.
+    Really Simplified.
+    """
+
+    pass
+
+
+class ListBlock(Block):
+    """
+    A class to represent a list block.
+    Really Simplified.
+    """
+
+    pass
+
+
+class HeaderBlock(Block):
+    """
+    A class to represent a header block.
+    Really Simplified.
+    """
+
+    pass
+
+
+class FooterBlock(Block):
+    """
+    A class to represent a footer block.
+    Really Simplified.
+    """
+
+    pass
 
 
 class Document(BaseModel):
@@ -11,5 +92,5 @@ class Document(BaseModel):
     """
 
     name: str
-    metadata: Dict
-    content: List[Element]
+    metadata: Dict  # TBD @Amine
+    content: List[Block]
